69f4cd8ae7524609dfb97359d2cbbc5293f745b1
gianluca cerritelli <gianluca.cerritelli@protolabs.co.uk>
2015-07-09 12:47:40 +0100

    Solution created

---
+++ CollectionPlanner/src/CollectionPlanner.Web/app-js/collectionPlanner.js
@@ -0,0 +1,9 @@
+﻿var collectionPlanner = angular.module('collectionPlanner', []);
+
+collectionPlanner.controller('StampsCatalogueController', ['$scope',
+    function($scope) {
+        $scope.collectibles = collectibles();
+        $scope.stampsCatalogue = $scope.collectibles.stamps;
+    }
+]);
+        
---
+++ CollectionPlanner/src/CollectionPlanner.Web/app-js/datastorage.js
@@ -0,0 +1,15 @@
+﻿'use strict';
+var collectibles = function () {
+    var stamps = [
+        { area: 'Italy', country: 'Tuscany', year: 1851, perforation: "imperf", color: "black", value: "1 quattrino", description: "Lion of Lorena", img: "A-s.jpg"},
+        { area: 'Italy', country: 'Tuscany', year: 1851, perforation: "imperf", color: "yellow", value: "1 soldo", description: "Lion of Lorena", img: "A1-s.jpg" },
+        { area: 'Italy', country: 'Tuscany', year: 1851, perforation: "imperf", color: "orange", value: "1 soldo", description: "Lion of Lorena", img: "A1-s.jpg" },
+        { area: 'Italy', country: 'Tuscany', year: 1851, perforation: "imperf", color: "scarlet", value: "2 soldi", description: "Lion of Lorena", img: "A3-s.jpg" },
+        { area: 'Italy', country: 'Tuscany', year: 1851, perforation: "imperf", color: "carmine red", value: "1 crazia", description: "Lion of Lorena", img: "A4-s.jpg" }
+    ];
+    
+    return {
+        stamps: stamps
+    };
+};
+
